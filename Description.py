5d2c492ea0f4025b3405efe84f16f1256df7faf2
emiryildirimm <emir67118@gmail.com>
2024-08-29 09:05:15 +0300

    Description added about application

---
+++ Description.py
@@ -0,0 +1,4 @@
+# --------------------------- Welcome user -------------------------
+# ----------This is a description about application ----------------
+
+print("This application provides this you can create your own secret notes")
